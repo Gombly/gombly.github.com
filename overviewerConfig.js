4b87cee780e05cbe9263043b929317655900863e
Christian Page <christian@localdesert.com>
2014-12-14 23:15:07 -0700

    rendered maps for Day, Night, Caves, and Nether

---
+++ overviewerConfig.js
@@ -0,0 +1,128 @@
+var overviewerConfig = {
+    "worlds": [
+        "Far Far Away - overworld", 
+        "Far Far Away - nether"
+    ], 
+    "tilesets": [
+        {
+            "spawn": [
+                449, 
+                96, 
+                321
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1418279027, 
+            "name": "Far Far Away Daytime", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 8, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "Far Far Away - overworld", 
+            "maxZoom": 8, 
+            "path": "Day", 
+            "showlocationmarker": true
+        }, 
+        {
+            "spawn": [
+                449, 
+                96, 
+                321
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1418279027, 
+            "name": "Far Far Away Nighttime", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 8, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "Far Far Away - overworld", 
+            "maxZoom": 8, 
+            "path": "Night", 
+            "showlocationmarker": true
+        }, 
+        {
+            "spawn": [
+                449, 
+                96, 
+                321
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1418279027, 
+            "name": "Far Far Away Caves", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 8, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "Far Far Away - overworld", 
+            "maxZoom": 8, 
+            "path": "Caves", 
+            "showlocationmarker": true
+        }, 
+        {
+            "spawn": "false", 
+            "isOverlay": false, 
+            "last_rendertime": 1418188815, 
+            "name": "Far Far Away Nether", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 6, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "Far Far Away - nether", 
+            "maxZoom": 6, 
+            "path": "Nether", 
+            "showlocationmarker": true
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "defaultMarker": "signpost.png", 
+            "queryMarker": "https://google-maps-icons.googlecode.com/files/regroup.png", 
+            "signMarker": "signpost_icon.png", 
+            "spawnMarker": "https://google-maps-icons.googlecode.com/files/home.png", 
+            "bedMarker": "bed.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3, 
+        "regionStrokeWeight": 2
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "mapType": true, 
+            "compass": true, 
+            "coordsBox": true, 
+            "overlays": true, 
+            "searchBox": true, 
+            "zoom": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1418622977"
+    }
+};
